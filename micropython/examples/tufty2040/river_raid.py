71407d2040970f38f76cce707d6fd8b94e3b0198
maurelioredfox <aureliomarcos777@gmail.com>
2023-03-29 11:49:51 -0300

    updated river raid

--- micropython/examples/tufty2040/river_raid.py
+++ micropython/examples/tufty2040/river_raid.py
@@ -7,18 +7,18 @@ display = display_singleton.get_display()
 WIDTH, HEIGHT = display.get_bounds() # 320 , 240
 
 BULLET_SPEED = 10
-PLAYER_SPEED = 6
+PLAYER_SPEED = 4
 MAP_SPEED = 2
 
 YELLOW = display.create_pen(255, 255, 0)
 
-TERRAINS = [0b00000011,0b00001111,0b00111111,0b11000011,0b00011111,0b00000111,0b11000011]
+TERRAINS = [0b00000011,0b00001111,0b00000111,0b11000011,0b00011111,0b00000111,0b11000011]
 
 BRIDGE_TERRAIN = 0b00111111
 PRE_BRIDGE_TERRAIN = 0b00000111
 
 spritesheet = bytearray(128 * 128)
-open("player.rgb332", "rb").readinto(spritesheet)
+open("river_raid.rgb332", "rb").readinto(spritesheet)
 
 # Buttons
 button_a = Button(7, invert=False)
@@ -43,6 +43,7 @@ class Player():
         self.fuel = 1000
         self.sprite = 2
         self.isAlive = True
+        self.score = 0
         
     def move(self,x,y):
         if self.x + x > 0 - self.width and self.x + x < WIDTH - self.width:
@@ -58,6 +59,8 @@ class Player():
         display.text("Fuel", 260, 200, scale=2)
         display.rectangle(260,220,round(50*(self.fuel/1000)),10)
         display.rectangle(260,215,50, 2)
+        display.text("Score", 260, 20, scale=2)
+        display.text(str(self.score), 260, 35, scale=2)
         
 class Shot():
     def __init__(self, x):
@@ -74,15 +77,16 @@ class Shot():
         display.line(self.x,self.y,self.x,self.y+7,4)
 
 class Target():
-    def __init__(self,x,y,width,height,limitX,limitY):
+    def __init__(self,x,y,width,height,Xmin,Xmax,score):
         self.isAlive = True
         self.x = x
         self.y = y
         self.width = width
         self.height = height
-        self.limitX = limitX
-        self.limitY = limitY
+        self.Xmin = Xmin
+        self.Xmax = Xmax
         self.is_Ally = False
+        self.score = score
         
     def should_delete(self):
         return (not self.isAlive) or self.y > 240
@@ -92,13 +96,13 @@ class Target():
     
     def draw(self):
         raise "not implemented"
-
+    
 class Bridge(Target):
     def __init__(self,y):
         self.road1 = display.create_pen(140,140,140)
         self.road2 = display.create_pen(180,180,180)
         self.road3 = display.create_pen(255,255,0)
-        super().__init__(120,y,80,28,0,0)
+        super().__init__(120,y,80,28,0,0,500)
         
         
     def should_delete(self):
@@ -122,7 +126,7 @@ class Bridge(Target):
         
 class Fuel(Target):
     def __init__(self,x,y):
-        super().__init__(x,y,24,56,0,0)
+        super().__init__(x,y,24,56,0,0,80)
         self.is_Ally = True
         
     def draw(self):
@@ -134,6 +138,98 @@ class Fuel(Target):
         display.sprite(1,3,self.x + 12,self.y + 16,2,0)
         display.sprite(1,4,self.x + 12,self.y + 32,2,0)
         display.sprite(1,5,self.x + 12,self.y + 48,2,0)
+    
+    
+class Boat(Target):
+    def __init__(self,x,y,Xmin,Xmax):
+        super().__init__(x,y,64,12,Xmin,Xmax,30)
+        self.direction = random.randint(0,1)
+        
+    def update(self):
+        self.y += MAP_SPEED
+        if not self.direction:
+            self.x -= 1
+            if self.x <= self.Xmin:
+                self.direction = 1
+        else:
+            self.x += 1
+            if self.x >= self.Xmax - self.width:
+                self.direction = 0
+        
+    def draw(self):
+        if not self.direction:
+            display.sprite(2,2,self.x     ,self.y,2,255)
+            display.sprite(3,2,self.x + 16,self.y,2,255)
+            display.sprite(4,2,self.x + 32,self.y,2,255)
+            display.sprite(5,2,self.x + 48,self.y,2,255)
+        else:
+            display.sprite(6,2,self.x     ,self.y,2,255)
+            display.sprite(7,2,self.x + 16,self.y,2,255)
+            display.sprite(8,2,self.x + 32,self.y,2,255)
+            display.sprite(9,2,self.x + 48,self.y,2,255)
+        
+        
+class Heli(Target):
+    def __init__(self,x,y,Xmin,Xmax):
+        super().__init__(x,y,32,12,Xmin,Xmax,60)
+        self.direction = random.randint(0,1)
+        self.helix = 0
+        
+    def update(self):
+        self.y += MAP_SPEED
+        if not self.direction:
+            self.x -= 1
+            if self.x <= self.Xmin:
+                self.direction = 1
+        else:
+            self.x += 1
+            if self.x >= self.Xmax - self.width:
+                self.direction = 0
+        self.helix += 1
+        if self.helix >= 10: self.helix = 0
+        
+    def draw(self):
+        if not self.direction:
+            display.sprite(4,4,self.x     ,self.y,2,255)
+            display.sprite(5,4,self.x + 16,self.y,2,255)
+        else:
+            display.sprite(2,4,self.x     ,self.y,2,255)
+            display.sprite(3,4,self.x + 16,self.y,2,255)
+        if self.helix < 5:
+            display.sprite(3,3,self.x + 8 + (self.direction * 12),self.y-4,2,255)
+            display.sprite(2,3,self.x     + (self.direction * 12),self.y-6,2,255)
+        else:
+            display.sprite(3,3,self.x + 8 + (self.direction * 12),self.y-6,2,255)
+            display.sprite(2,3,self.x     + (self.direction * 12),self.y-4,2,255)
+        
+        
+class Drone(Target):
+    def __init__(self,y):
+        super().__init__(0,y,64,12,0,0,100)
+        self.direction = random.randint(0,1)
+        if not self.direction:
+            self.x = 400
+        else:
+            self.x = -20
+        self.active = False
+        
+    def update(self):
+        self.y += MAP_SPEED
+        if not self.active and self.y > 0 and random.randint(0,20) == 20:
+            self.active = True
+        elif self.active:
+            if not self.direction:
+                self.x -= 2
+            else:
+                self.x += 2
+        
+    def draw(self):
+        if not self.direction:
+            display.sprite(6,3,self.x     ,self.y,2,255)
+            display.sprite(7,3,self.x + 16,self.y,2,255)
+        else:
+            display.sprite(6,4,self.x + 24,self.y,2,255)
+            display.sprite(7,4,self.x + 32,self.y,2,255)
 
 class Game():
     
@@ -150,6 +246,10 @@ class Game():
         self.terrain.append(TERRAINS[0])
         self.terrain.append(TERRAINS[2])
         self.terrain.append(TERRAINS[2])
+        self.targets.append(Heli(120,0,40,280))
+        self.targets.append(Boat(242,-150,60,260))
+        self.targets.append(Heli(140,-200,60,260))
+        self.targets.append(Boat(160,-240,60,260))
         self.terrain_offset = 0
         self.terrain_number = 5
         self.new_terrain = False
@@ -171,6 +271,7 @@ class Game():
                         
     #read movements
     def process_input(self):
+        global MAP_SPEED
         if button_b.raw():
             self.player.move(PLAYER_SPEED, 0)
             self.player.sprite = 4
@@ -181,8 +282,15 @@ class Game():
             self.player.sprite = 2
         if button_c.read() and len(self.bullets) == 0:
             self.bullets.append(Shot(self.player.x))
+        if button_up.raw():
+            MAP_SPEED = 3
+        elif button_down.raw():
+            MAP_SPEED = 1
+        else:
+            MAP_SPEED = 2
            
     def update_terrain(self):
+        print(MAP_SPEED)
         self.terrain_offset += MAP_SPEED
         if(self.terrain_offset >= 120):
             self.new_terrain = True
@@ -239,10 +347,22 @@ class Game():
                     else:
                         max_x = 120
                 x = random.randint(min_x,max_x - 32)
-                y = random.randint(-220,-170)
+                y = random.randint(-280,-240)
                 self.targets.append(Fuel(x,y))
                 print('terrain was',bin(terrain),'fuel is on',x,y)
             #decide for enemies + level
+            if random.randint(0,1) and not bifurcated:
+                x = random.randint(min_x,max_x-64)
+                y = random.randint(-280,-180)
+                self.targets.append(Boat(x,y,min_x,max_x))
+                print("boat",x,y,min_x,max_x,bin(terrain))
+            if random.randint(0,1):
+                x = random.randint(min_x,max_x-32)
+                y = random.randint(-280,-180)
+                self.targets.append(Heli(x,y,min_x,max_x))
+            if random.randint(0,1):
+                y = random.randint(-280,-180)
+                self.targets.append(Drone(y))
         pass
     
     def update(self):
@@ -251,10 +371,13 @@ class Game():
             bullet.update()
             if(bullet.y < 0):
                 self.bullets.remove(bullet)
+                continue
             for target in self.targets:
                 if target.isAlive and self.check_collision(bullet,target):
                     self.bullets.remove(bullet)
                     target.isAlive = False
+                    self.player.score += target.score
+                    continue
         #terrain
         self.update_terrain()
         self.check_collision_terrain()
@@ -281,12 +404,14 @@ class Game():
         display.set_pen(self.TERRAIN_PEN)
         
         for y,terrain in enumerate(self.terrain):
+            display.set_pen(self.TERRAIN_PEN)
             coord_y = 200 - (y*120) + self.terrain_offset
             for x in range(0,8):
                 if(terrain & (1 << x)):
                     display.rectangle(300-(20*x),coord_y, 20, 100)
                     display.rectangle(     20*x ,coord_y, 20, 100)
             if(coord_y > 0):
+                display.set_pen(display.create_pen(40, 190, 40))
                 dif =  abs((terrain & 0b111111) - (self.terrain[y+1] & 0b111111))
                 match = terrain & self.terrain[y+1]
                 xhign = 0
